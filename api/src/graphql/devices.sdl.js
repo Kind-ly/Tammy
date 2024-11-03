518d054dfe77991a0addb9e5f97abb8ca5f2625c
Darryl <dazsnow@gmail.com>
2024-11-03 15:29:14 +0800

    random 10-digit device id

--- api/src/graphql/devices.sdl.js
+++ api/src/graphql/devices.sdl.js
@@ -11,6 +11,7 @@ export const schema = gql`
   }
 
   input CreateDeviceInput {
+    id: Int!
     careGiverId: String!
   }
 
--- api/src/services/devices/devices.js
+++ api/src/services/devices/devices.js
@@ -12,7 +12,11 @@ export const device = ({ id }) => {
 
 export const createDevice = ({ input }) => {
   return db.device.create({
-    data: input,
+    data: {
+      ...input,
+      id:
+        Math.floor(Math.random() * (9999999999 - 1000000000 + 1)) + 1000000000,
+    },
   })
 }
 
