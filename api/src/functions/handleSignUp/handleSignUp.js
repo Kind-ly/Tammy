0f1e525e6d530f1f8a5e90af31d946c449acb239
Darryl <dazsnow@gmail.com>
2025-01-11 16:39:28 +0800

    add phone number to handleSignUp function

--- api/src/functions/handleSignUp/handleSignUp.js
+++ api/src/functions/handleSignUp/handleSignUp.js
@@ -20,7 +20,7 @@ const register = async (user) => {
         role,
         email: user.email,
         name: user.user_metadata.full_name,
-        phoneNumber: null,
+        phoneNumber: '',
       },
     })
     // console.log('newUser function')
